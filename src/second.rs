f6bfab4219d7b6010779f7a79dacb2e8d7e0741c
rodrigo debian <correo.rodrigo@gmail.com>
2021-03-30 15:35:48 -0600

    test for Iter

--- src/second.rs
+++ src/second.rs
@@ -158,4 +158,17 @@ mod test {
         assert_eq!(iter.next(), None);
         assert_eq!(iter.next(), None);
     }
+
+    #[test]
+    fn iter() {
+        let mut list = List::new();
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        let mut iter = list.iter();
+        assert_eq!(iter.next(), Some(&3));
+        assert_eq!(iter.next(), Some(&2));
+        assert_eq!(iter.next(), Some(&1));
+    }
 }
