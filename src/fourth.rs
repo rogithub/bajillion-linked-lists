7d6dd73749090f75074ff4d5428cc8330bdf0c84
rodrigo debian <correo.rodrigo@gmail.com>
2021-04-02 12:58:55 -0600

    into iter

--- src/fourth.rs
+++ src/fourth.rs
@@ -1,6 +1,6 @@
 use core::cell::RefMut;
-use std::rc::Rc;
 use std::cell::{Ref, RefCell};
+use std::rc::Rc;
 
 pub struct List<T> {
     head: Link<T>,
@@ -33,14 +33,17 @@ impl<T> Drop for List<T> {
 
 impl<T> List<T> {
     pub fn new() -> Self {
-        List { head: None, tail: None }
+        List {
+            head: None,
+            tail: None,
+        }
     }
 
-    pub fn peek_front(&self) -> Option<Ref<T>> {        
-        self.head.as_ref().map(|node| {
-            Ref::map(node.borrow(), |node| &node.elem)
-        })
-    }    
+    pub fn peek_front(&self) -> Option<Ref<T>> {
+        self.head
+            .as_ref()
+            .map(|node| Ref::map(node.borrow(), |node| &node.elem))
+    }
 
     pub fn push_front(&mut self, elem: T) {
         // new node needs +2 links, everything else should be +0
@@ -49,40 +52,40 @@ impl<T> List<T> {
             Some(old_head) => {
                 // non-empty list, need to connect the old_head
                 old_head.borrow_mut().prev = Some(new_head.clone()); // +1 new_head
-                new_head.borrow_mut().next = Some(old_head);         // +1 old_head
-                self.head = Some(new_head);             // +1 new_head, -1 old_head
-                // total: +2 new_head, +0 old_head -- OK!
+                new_head.borrow_mut().next = Some(old_head); // +1 old_head
+                self.head = Some(new_head); // +1 new_head, -1 old_head
+                                            // total: +2 new_head, +0 old_head -- OK!
             }
             None => {
                 // empty list, need to set the tail
-                self.tail = Some(new_head.clone());     // +1 new_head
-                self.head = Some(new_head);             // +1 new_head
-                // total: +2 new_head -- OK!
+                self.tail = Some(new_head.clone()); // +1 new_head
+                self.head = Some(new_head); // +1 new_head
+                                            // total: +2 new_head -- OK!
             }
         }
     }
 
     pub fn pop_front(&mut self) -> Option<T> {
         // need to take the old head, ensuring it's -2
-        self.head.take().map(|old_head| {                         // -1 old
+        self.head.take().map(|old_head| {
+            // -1 old
             match old_head.borrow_mut().next.take() {
-                Some(new_head) => {                               // -1 new
+                Some(new_head) => {
+                    // -1 new
                     // not emptying list
-                    new_head.borrow_mut().prev.take();            // -1 old
-                    self.head = Some(new_head);                   // +1 new
-                    // total: -2 old, +0 new
+                    new_head.borrow_mut().prev.take(); // -1 old
+                    self.head = Some(new_head); // +1 new
+                                                // total: -2 old, +0 new
                 }
                 None => {
                     // emptying list
-                    self.tail.take();                             // -1 old
-                    // total: -2 old, (no new)
+                    self.tail.take(); // -1 old
+                                      // total: -2 old, (no new)
                 }
             }
-            
             Rc::try_unwrap(old_head).ok().unwrap().into_inner().elem
         })
-    }    
-
+    }
 
     pub fn push_back(&mut self, elem: T) {
         let new_tail = Node::new(elem);
@@ -98,7 +101,6 @@ impl<T> List<T> {
             }
         }
     }
-    
     pub fn pop_back(&mut self) -> Option<T> {
         self.tail.take().map(|old_tail| {
             match old_tail.borrow_mut().prev.take() {
@@ -113,25 +115,40 @@ impl<T> List<T> {
             Rc::try_unwrap(old_tail).ok().unwrap().into_inner().elem
         })
     }
-    
     pub fn peek_back(&self) -> Option<Ref<T>> {
-        self.tail.as_ref().map(|node| {
-            Ref::map(node.borrow(), |node| &node.elem)
-        })
+        self.tail
+            .as_ref()
+            .map(|node| Ref::map(node.borrow(), |node| &node.elem))
     }
-    
     pub fn peek_back_mut(&mut self) -> Option<RefMut<T>> {
-        self.tail.as_ref().map(|node| {
-            RefMut::map(node.borrow_mut(), |node| &mut node.elem)
-        })
+        self.tail
+            .as_ref()
+            .map(|node| RefMut::map(node.borrow_mut(), |node| &mut node.elem))
     }
-    
     pub fn peek_front_mut(&mut self) -> Option<RefMut<T>> {
-        self.head.as_ref().map(|node| {
-            RefMut::map(node.borrow_mut(), |node| &mut node.elem)
-        })
+        self.head
+            .as_ref()
+            .map(|node| RefMut::map(node.borrow_mut(), |node| &mut node.elem))
     }
-    
+
+    pub fn into_iter(self) -> IntoIter<T> {
+        IntoIter(self)
+    }
+}
+
+pub struct IntoIter<T>(List<T>);
+
+impl<T> Iterator for IntoIter<T> {
+    type Item = T;
+    fn next(&mut self) -> Option<T> {
+        self.0.pop_front()
+    }
+}
+
+impl<T> DoubleEndedIterator for IntoIter<T> {
+    fn next_back(&mut self) -> Option<T> {
+        self.0.pop_back()
+    }
 }
 
 #[cfg(test)]
@@ -201,7 +218,9 @@ mod test {
         assert!(list.peek_front_mut().is_none());
         assert!(list.peek_back_mut().is_none());
 
-        list.push_front(1); list.push_front(2); list.push_front(3);
+        list.push_front(1);
+        list.push_front(2);
+        list.push_front(3);
 
         assert_eq!(&*list.peek_front().unwrap(), &3);
         assert_eq!(&mut *list.peek_front_mut().unwrap(), &mut 3);
@@ -209,5 +228,18 @@ mod test {
         assert_eq!(&mut *list.peek_back_mut().unwrap(), &mut 1);
     }
 
+    #[test]
+    fn into_iter() {
+        let mut list = List::new();
+        list.push_front(1);
+        list.push_front(2);
+        list.push_front(3);
 
+        let mut iter = list.into_iter();
+        assert_eq!(iter.next(), Some(3));
+        assert_eq!(iter.next_back(), Some(1));
+        assert_eq!(iter.next(), Some(2));
+        assert_eq!(iter.next_back(), None);
+        assert_eq!(iter.next(), None);
+    }
 }
